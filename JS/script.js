e90b7a92bedc66a2907056b29d9a0f0cecceec46
Naomi Chika <naomichika@marias-MacBook.local>
2021-02-19 17:11:16 +0300

    paste JS script given in slack

---
+++ JS/script.js
@@ -0,0 +1,89 @@
+function fn(){
+  var d = new Date();
+  var date = parseInt(document.getElementById("day").value);
+  var month = parseInt(document.getElementById("month").value) - 1;
+  var year = parseInt(document.getElementById("year").value);
+  if (day < 0 || day > 31) {
+      document.getElementsByClassName("dayerror").innerHtml = "Enter valid date";
+      return false;
+  } else if (date == "") {
+      document.getElementsByClassName("dayerror").innerHtml = "This field cannot be empty";
+      return false;
+  } else {
+      d.setDate(date);
+  }
+  if (month < 0 || day > 12) {
+      document.getElementsByClassName("montherror").innerHtml = "Enter valid month";
+      return false;
+  } else if (month == "") {
+      document.getElementsByClassName("montherror").innerHtml = "This field cannot be empty";
+      return false;
+  } else {
+      d.setMonth(month);
+  }
+  if (month < 0) {
+      document.getElementsByClassName("yearerror").innerHtml = "Enter valid year";
+      return false;
+  } else if (month == "") {
+      document.getElementsByClassName("yearerror").innerHtml = "This field cannot be empty";
+      return false;
+  } else {
+      d.setYear(year);
+  }
+  var day = d.getDay();
+  var days = ['sunday', 'monday', "tuesday", "wednesday", "thursday", "friday", "saturday"]
+  var male = ["Kwasi", " Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+function getAkan() {
+  var ismale = document.getElementById('male').checked;
+  var isfemale = document.getElementById('female').checked;
+  if (ismale == true && isfemale == false) {
+      if (day == 0) {
+          // return male[0];
+          alert("your name is " + male[0]);
+      }
+      else if (day == 1) {
+          alert("your name is " + male[1]);
+      }
+      else if (day == 2) {
+          alert("your name is " + male[2]);
+      }
+      else if (day == 3) {
+          alert("your name is " + male[3]);
+      }
+      else if (day == 4) {
+          alert("your name is " + male[4]);
+      }
+      else if (day == 5) {
+          alert("your name is " + male[5]);
+      }
+      else if (day == 6) {
+          alert("your name is " + male[6]);
+      }
+  } else if (ismale == false && isfemale == true) {
+      if (day == 0) {
+          // return male[0];
+          alert("your name is " + female[0]);
+      }
+      else if (day == 1) {
+          alert("your name is " + female[1]);
+      }
+      else if (day == 2) {
+          alert("your name is " + female[2]);
+      }
+      else if (day == 3) {
+          alert("your name is " + female[3]);
+      }
+      else if (day == 4) {
+          alert("your name is " + female[4]);
+      }
+      else if (day == 5) {
+          alert("your name is " + female[5]);
+      }
+      else if (day == 6) {
+          alert("your name is " + female[6]);
+      }
+  }
+  }
+  getAkan();
+}
